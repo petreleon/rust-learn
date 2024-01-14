fb8e599d5f2d01d966f199ffc3e4735bbe0fbe15
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-01-14 19:49:53 +0200

    add database in main

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ pub mod api;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
 use infer::Infer;
 
+use diesel::r2d2::{self, ConnectionManager};
+use diesel::PgConnection;
+use std::env;
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -39,8 +43,17 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenvy::dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: DbPool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/hey", web::get().to(manual_hello))
             .service(api::api_scope())
             .service(hello)
