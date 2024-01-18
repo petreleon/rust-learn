204a329516d6d8013cb3c3adca9079bf14c59aa7
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-01-18 02:22:54 +0200

    add decode jwt

--- src/utils/jwt_utils.rs
+++ src/utils/jwt_utils.rs
@@ -4,6 +4,7 @@ use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
 use std::env;
 use chrono;
 use crate::models::user_jwt::UserJWT;
+use jsonwebtoken::{decode, DecodingKey, Validation, TokenData};
 
 
 pub fn create_jwt(user_id: i32) -> Result<String, jsonwebtoken::errors::Error> {
@@ -15,4 +16,11 @@ pub fn create_jwt(user_id: i32) -> Result<String, jsonwebtoken::errors::Error> {
     );
     let encoding_key = EncodingKey::from_rsa_pem(private_key.as_bytes())?;
     encode(&Header::new(Algorithm::RS256), &claims, &encoding_key)
+}
+
+pub fn decode_jwt(token: &str) -> Result<TokenData<UserJWT>, jsonwebtoken::errors::Error> {
+    let public_key = env::var("PUBLIC_KEY").expect("PUBLIC_KEY must be set");
+    let decoding_key = DecodingKey::from_rsa_pem(public_key.as_bytes())?;
+    let validation = Validation::new(Algorithm::RS256);
+    decode::<UserJWT>(token, &decoding_key, &validation)
 }
