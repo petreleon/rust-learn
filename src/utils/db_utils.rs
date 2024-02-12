9d338932c4b76b8c3c79825816b0352bc8ae19fe
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-02-12 16:21:11 +0200

    user comparing

--- src/utils/db_utils.rs
+++ src/utils/db_utils.rs
@@ -2,7 +2,8 @@ use diesel::{dsl::{any, count}, prelude::*, sql_types::{Int4, Integer}};
 use diesel::sql_types::Nullable;
 
 use crate::db::schema::role_permission_organization; 
-
+use diesel::dsl::{min, exists};
+use std::cmp::Ordering;
 // Checks if a user has a specific permission on the platform
 use crate::db::schema::*;
 use diesel::prelude::*;
@@ -48,4 +49,62 @@ fn user_permission_organization_request(
     .get_result(conn)?;
 
     Ok(has_permission)
+}
+
+
+fn user_hierarchy_compare_platform(
+    conn: &mut PgConnection,
+    user1_id: i32,
+    user2_id: i32,
+) -> QueryResult<Ordering> {
+    use crate::db::schema::{role_platform_hierarchy, user_role_platform};
+
+    let user1_min_level = role_platform_hierarchy::table
+        .inner_join(user_role_platform::table.on(role_platform_hierarchy::role_id.eq(user_role_platform::role_id)))
+        .filter(user_role_platform::user_id.eq(user1_id))
+        .select(min(role_platform_hierarchy::hierarchy_level))
+        .first::<Option<i32>>(conn)?;
+
+    let user2_min_level = role_platform_hierarchy::table
+        .inner_join(user_role_platform::table.on(role_platform_hierarchy::role_id.eq(user_role_platform::role_id)))
+        .filter(user_role_platform::user_id.eq(user2_id))
+        .select(min(role_platform_hierarchy::hierarchy_level))
+        .first::<Option<i32>>(conn)?;
+
+    match (user1_min_level, user2_min_level) {
+        (Some(level1), Some(level2)) => Ok(level2.cmp(&level1)),
+        (None, None) => Ok(Ordering::Equal),
+        (Some(_), None) => Ok(Ordering::Greater), // User with a role is considered 'higher'
+        (None, Some(_)) => Ok(Ordering::Less),
+    }
+}
+
+fn user_hierarchy_compare_organization(
+    conn: &mut PgConnection,
+    organization_id: i32,
+    user1_id: i32,
+    user2_id: i32,
+) -> QueryResult<Ordering> {
+    use crate::db::schema::{role_organization_hierarchy, user_role_organization};
+
+    let user1_min_level = role_organization_hierarchy::table
+        .inner_join(user_role_organization::table.on(role_organization_hierarchy::role_id.eq(user_role_organization::role_id)))
+        .filter(user_role_organization::user_id.eq(user1_id))
+        .filter(user_role_organization::organization_id.eq(organization_id))
+        .select(min(role_organization_hierarchy::hierarchy_level))
+        .first::<Option<i32>>(conn)?;
+
+    let user2_min_level = role_organization_hierarchy::table
+        .inner_join(user_role_organization::table.on(role_organization_hierarchy::role_id.eq(user_role_organization::role_id)))
+        .filter(user_role_organization::user_id.eq(user2_id))
+        .filter(user_role_organization::organization_id.eq(organization_id))
+        .select(min(role_organization_hierarchy::hierarchy_level))
+        .first::<Option<i32>>(conn)?;
+
+    match (user1_min_level, user2_min_level) {
+        (Some(level1), Some(level2)) => Ok(level2.cmp(&level1)),
+        (None, None) => Ok(Ordering::Equal),
+        (Some(_), None) => Ok(Ordering::Greater), 
+        (None, Some(_)) => Ok(Ordering::Less),
+    }
 }
