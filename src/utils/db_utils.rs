bb5fddfae2adf171a23310d854663e8b06e3c8cf
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-03-06 20:15:02 +0200

    suggestive renaming of a variable

--- src/utils/db_utils.rs
+++ src/utils/db_utils.rs
@@ -76,21 +76,21 @@ pub fn user_hierarchy_compare_organization(
     user2_id: i32,
 ) -> QueryResult<Ordering> {
 
-    let user1_min_level = role_organization_hierarchy::table
+    let user1_max_level = role_organization_hierarchy::table
         .inner_join(user_role_organization::table.on(role_organization_hierarchy::role_id.eq(user_role_organization::role_id)))
         .filter(user_role_organization::user_id.eq(user1_id))
         .filter(user_role_organization::organization_id.eq(organization_id))
         .select(min(role_organization_hierarchy::hierarchy_level))
         .first::<Option<i32>>(conn)?;
 
-    let user2_min_level = role_organization_hierarchy::table
+    let user2_max_level = role_organization_hierarchy::table
         .inner_join(user_role_organization::table.on(role_organization_hierarchy::role_id.eq(user_role_organization::role_id)))
         .filter(user_role_organization::user_id.eq(user2_id))
         .filter(user_role_organization::organization_id.eq(organization_id))
         .select(min(role_organization_hierarchy::hierarchy_level))
         .first::<Option<i32>>(conn)?;
 
-    match (user1_min_level, user2_min_level) {
+    match (user1_max_level, user2_max_level) {
         (Some(level1), Some(level2)) => Ok(level2.cmp(&level1)),
         (None, None) => Ok(Ordering::Equal),
         (Some(_), None) => Ok(Ordering::Greater), 
