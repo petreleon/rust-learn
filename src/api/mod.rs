6c7298c60dbdb2f7ba97ff5e565046c9cd05b3ce
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-03-16 08:32:19 +0200

    use connection

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,12 +4,14 @@ pub mod authentication;
 use actix_service::ServiceFactory;
 use actix_web::{Scope, dev::ServiceRequest, dev::ServiceResponse, Error};
 
-use crate::middlewares::jwt_middleware::JwtMiddleware;
+use crate::middlewares::{conditional_access_middleware::ConditionalAccessMiddleware, jwt_middleware::JwtMiddleware};
+use crate::middlewares::db_connection_middleware::{DB_CONNECTION_MIDDLEWARE_CONDITION, DB_CONNECTION_MIDDLEWARE_ERROR};
 use actix_web::web;
 
 pub fn api_scope() -> Scope<impl ServiceFactory<ServiceRequest, Config = (), Response = ServiceResponse, Error = Error, InitError = ()>> {
     web::scope("/api")
         .wrap(JwtMiddleware)
+        .wrap(ConditionalAccessMiddleware::new(DB_CONNECTION_MIDDLEWARE_CONDITION, DB_CONNECTION_MIDDLEWARE_ERROR))
         .service(users::user_scope())
         .service(authentication::auth_scope())
         // Add more API services here...
--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -4,3 +4,4 @@ pub mod organization_hierarchy_middleware;
 pub mod platform_permission_middleware;
 pub mod organization_permission_middleware;
 pub mod conditional_access_middleware;
+pub mod db_connection_middleware;
