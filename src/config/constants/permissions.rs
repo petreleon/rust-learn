98f718108b2afca1b9cf7546258b15e499fdfb95
Petre Leonard Macamete <petreleonardos@gmail.com>
2024-03-16 08:33:30 +0200

    Extend list of permissions

--- src/config/constants/permissions.rs
+++ src/config/constants/permissions.rs
@@ -1,16 +1,59 @@
 use strum_macros::{Display, EnumString};
-// TODO: Add the permissions that are going to be used in the application
 
 #[derive(Display, EnumString, Debug, PartialEq)]
 #[allow(non_camel_case_types)]
-pub enum Permissions{
+pub enum Permissions {
+    // Organization-related permissions
     CREATE_ORGANIZATION,
-    MODIFY_ANY_ORGANIZATION,
+    MODIFY_ORGANIZATION,
+    DELETE_ORGANIZATION,
+    VIEW_ORGANIZATION,
+    REQUEST_JOIN_ORGANIZATION,
+    ACCEPT_ORGANIZATION_JOIN_REQUEST,
+
+    // Course-related permissions
     CREATE_COURSE,
+    MODIFY_COURSE,
+    DELETE_COURSE,
+    VIEW_COURSE,
     JOIN_COURSE,
     REQUEST_JOIN_COURSE,
-    MODIFY_ANY_COURSE,
-    MODIFY_ANY_USER,
-    MODIFY_ANY_ROLE,
-    MODIFY_ANY_PERMISSION,
+    APPROVE_COURSE_JOIN_REQUESTS,
+
+    // User management
+    MODIFY_USER,
+    DELETE_USER,
+    VIEW_USER,
+    INVITE_USER_TO_ORGANIZATION,
+
+    // Role and permissions management
+    MODIFY_ROLE,
+    DELETE_ROLE,
+    ASSIGN_ROLES_TO_USER,
+    MODIFY_PERMISSION,
+
+    // Content management
+    CREATE_CONTENT,
+    MODIFY_CONTENT,
+    DELETE_CONTENT,
+    VIEW_CONTENT,
+
+    // Assessment and evaluations
+    CREATE_ASSESSMENT,
+    MODIFY_ASSESSMENT,
+    DELETE_ASSESSMENT,
+    GRADE_ASSESSMENT,
+    VIEW_ASSESSMENT,
+
+    // Discussions and forums
+    POST_IN_DISCUSSION,
+    MODERATE_DISCUSSION,
+
+    // Notifications
+    SEND_NOTIFICATION,
+    VIEW_NOTIFICATION,
+
+    // Analytics and reports
+    VIEW_REPORT,
+    GENERATE_REPORT,
 }
