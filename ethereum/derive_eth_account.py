77af2eb6b5400a1e46ec8e2c580c795d2e041683
Petre-Leonard Macamete <petreleonardos@gmail.com>
2025-07-30 13:36:14 +0300

    add geth to development

---
+++ ethereum/derive_eth_account.py
@@ -0,0 +1,15 @@
+from eth_account import Account
+import os
+import sys
+
+def main():
+    mnemonic = os.environ.get('ETH_MNEMONIC')
+    if not mnemonic:
+        print('ETH_MNEMONIC not set.', file=sys.stderr)
+        sys.exit(1)
+    acct = Account.from_mnemonic(mnemonic)
+    print(acct.address)
+    print(acct.key.hex())
+
+if __name__ == "__main__":
+    main()
