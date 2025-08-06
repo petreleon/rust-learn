08f08cdcf1cb39123666e7627efc55f8ffa1d46a
Petre Leonard Macamete <petreleonardos@gmail.com>
2025-08-06 20:52:27 +0000

    Add LearnToken contract with mint and burn functionality

---
+++ ethereum/contracts/LearnToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LearnToken is ERC20, Ownable {
+    uint8 private _customDecimals;
+
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _customDecimals = decimals_;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _customDecimals;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
