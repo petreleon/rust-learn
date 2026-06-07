5135e53f574766ca4584742ea7dfd7d18a3e048b
Petre Leonard Macamete <petreleonardos@gmail.com>
2026-06-07 12:54:20 +0300

    Hide Next dev indicator in web console

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,7 @@ const apiUrl = process.env.API_URL || "http://127.0.0.1:8080";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "::1"],
+  devIndicators: false,
   async rewrites() {
     return [
       {
